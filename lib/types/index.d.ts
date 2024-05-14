e80239e0aa75359a0504c359abb67fae79a06790
Jonathan Delgado <hi@jon.soy>
2024-05-13 20:08:51 -0400

    build: rename package and add script prepack

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -3,11 +3,9 @@
  */
 export declare const SymbolInitialize: unique symbol;
 export type Initializable = {
-  [SymbolInitialize](): Promise<any>;
+    [SymbolInitialize](): Promise<any>;
 };
-export declare const isInitializable: (
-  value: unknown,
-) => value is Initializable;
+export declare const isInitializable: (value: unknown) => value is Initializable;
 export declare const initializeOne: (value: unknown) => Promise<any> | null;
 export declare const initialize: <T>(object: T) => Promise<T>;
 export declare const isInitialized: (object: unknown) => boolean;
